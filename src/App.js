565987940ec8e8756ed7ca3d43050e070441e6ff
ncilliez <ncilliez@gmail.com>
2023-02-16 15:18:17 +0100

    teste

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useEffect, useState } from "react";
+
+const lang = 'french';
+const url = `https://restcountries.com/v3.1/lang/${lang}`;
+
 function App() {
+  const [table, setTable] = useState([]);
+  // const [selection, setSelection] = useState('french');
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+
+        setTable(data);
+
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+  }, []);
+
+  
+
+  const select =()=>{
+    var info = document.getElementById('filtre').value
+    console.log(info)
+  };
+  console.log(table);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" id="filtre"/>
+      <button onClick ={() => select()}>Valider</button>
+      
+      <ul>
+        {table.map((pays, i) => {
+          return (
+            <li key={i}>{pays.name.common}</li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
